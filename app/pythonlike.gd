9b666915a0fd3515183bb8b17aac4fac6fc4a686
J <justintime155@gmail.com>
2022-09-09 02:02:00 -0400

    made triangle stop

--- app/pythonlike.gd
+++ app/pythonlike.gd
@@ -6,18 +6,26 @@ var lastposition : Vector2
 
 @export var Triangle : Node2D
 
-
+var boundary : Vector2i
 
 func _ready():
 	print("Hello World") 
 	print(Triangle.name)
-	Velocity.x = 1
+	Velocity.x = 100
+	Velocity.y = 25
 	lastposition = Triangle.global_position
-	
+	get_tree().get_root() # access via scenemainloop
+	print(get_tree().get_root().size)
+	boundary.x = 700 #1152 base
+	boundary.y = 348 #648 base
 
 func _process(delta : float):
 	print(Triangle.name)
 	print(Triangle.global_position)
+	if Triangle.global_position.x >= boundary.x or Triangle.global_position.y >= boundary.y:
+		Velocity.x = 0
+		Velocity.y = 0
+	
 	
 #	Triangle.global_position = Vector2(0,0) 
 #	Triangle.global_position = Triangle.global_position + Velocity  reg=reg*reg aids
@@ -26,4 +34,4 @@ func _process(delta : float):
 #	Triangle.global_position = lastposition + Velocity
 #	lastposition = Triangle.global_position
 
-#	Triangle.global_position+=Velocity  short hand
+	Triangle.global_position += Velocity * delta #short hand
