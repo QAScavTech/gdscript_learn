3ade137c96cb9b13932be21062bf498eb0507012
J <justintime155@gmail.com>
2022-09-11 00:52:20 -0400

    Made triangle stop on border

--- app/pythonlike.gd
+++ app/pythonlike.gd
@@ -4,27 +4,58 @@ var Velocity : Vector2
 
 var lastposition : Vector2
 
-@export var Triangle : Node2D
+var polygon : PackedVector2Array
+
+@export var Triangle : Polygon2D
 
 var boundary : Vector2i
 
 func _ready():
 	print("Hello World") 
 	print(Triangle.name)
-	Velocity.x = 100
-	Velocity.y = 25
+	Velocity.x = 125
+	Velocity.y = 100
 	lastposition = Triangle.global_position
 	get_tree().get_root() # access via scenemainloop
 	print(get_tree().get_root().size)
-	boundary.x = 700 #1152 base
-	boundary.y = 348 #648 base
+	boundary.x = 1152 #1152 base
+	boundary.y = 648 #648 base
+	print(Triangle.polygon)
+	
+	#var index : int = 0
+	var index := 0 #shorthand for above. This assigns type and value in same line.
+	
+	while index < Triangle.polygon.size():
+		print(Triangle.polygon[index])
+#		index = index + 1
+		index += 1 #shorthand for above
+	
+	
+	
 
 func _process(delta : float):
 	print(Triangle.name)
 	print(Triangle.global_position)
-	if Triangle.global_position.x >= boundary.x or Triangle.global_position.y >= boundary.y:
-		Velocity.x = 0
-		Velocity.y = 0
+	
+	var Pivot := Triangle.global_position
+	
+	var index := 0
+	
+	while index < Triangle.polygon.size():
+		
+		print(Triangle.polygon[index])
+		
+		var point = Triangle.polygon[index] + Pivot
+		
+		if point.x >= boundary.x:
+			Velocity.x = 0
+		
+		if point.y >= boundary.y:
+			Velocity.y = 0
+			
+		index += 1
+	
+
 	
 	
 #	Triangle.global_position = Vector2(0,0) 
